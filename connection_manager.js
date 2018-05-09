e4a8a72df581493745814b3539fedd51a316448d
lotsofone <552694725@qq.com>
2018-05-09 13:50:27 +0800

    把连接部分模块化

---
+++ connection_manager.js
@@ -0,0 +1,86 @@
+var connection_manager = {};
+connection_manager.server_socket = null;
+connection_manager.peerConnection = null;
+connection_manager.dataChannel = null;
+
+connection_manager.socketServer = null;
+connection_manager.iceServers = [];
+connection_manager.tagFuncMap = new Map();
+
+connection_manager.startSocket = function(){
+    this.server_socket = new WebSocket("wss://"+this.socketServer);
+    this.server_socket.onmessage = function(e){connection_manager.distribute(e);};
+    return this.server_socket;
+}
+connection_manager.closeSocket = function(){
+    this.server_socket.close();
+    this.server_socket = null;
+}
+connection_manager.distribute = function(e){
+    var edata = e.data;
+    if(e.data.charAt(0)=='>'){
+        edata = e.data.substring(1, e.data.length);
+    }
+    var message = JSON.parse(edata);
+    var func = connection_manager.tagFuncMap.get(message.tag);
+    if(func){
+        func(message);
+    }
+    else{
+        console.log("Unlistened tag: "+message.tag);
+    }
+}
+connection_manager.setDistributionFunction = function(tag, func){
+    if(func){
+        this.tagFuncMap.set(tag, func);
+    }
+    else{
+        this.tagFuncMap.delete(tag);
+    }
+}
+connection_manager.startPeerConnection = function(starter){
+    var option = {iceServers: this.iceServers};
+    this.peerConnection = new RTCPeerConnection(option);
+    var dataoption = {ordered: false, maxRetransmitTime: 1, negotiated: true, id: 1};
+    this.dataChannel = this.peerConnection.createDataChannel("gm", dataoption);
+    if(starter){//to be the one to createOffer
+        this.peerConnection.createOffer({offerToReceiveAudio: false, offerToReceiveVideo: false, voiceActivityDetection: false})
+        .then(function(offer){
+            connection_manager.peerConnection.setLocalDescription(offer);
+            connection_manager.setDistributionFunction("answer", function(message){//listen to "answer" message to receive answer
+                connection_manager.peerConnection.setRemoteDescription(message.answer);
+                connection_manager.setDistributionFunction("answer", null);//no need to listen to "answer" message anynore
+            });
+            connection_manager.server_socket.send(">"+JSON.stringify({tag: "offer", offer: offer}));
+        });
+    }
+    else{//to be the one to wait an offer and createAnswer
+        this.setDistributionFunction("offer", function(message){//listen to "offer" message to receive offer
+            connection_manager.peerConnection.setRemoteDescription(message.offer)
+            .then(function(){
+                connection_manager.peerConnection.createAnswer({offerToReceiveAudio: false, offerToReceiveVideo: false, voiceActivityDetection: false})
+                .then(function(answer){
+                    connection_manager.peerConnection.setLocalDescription(answer);
+                    connection_manager.setDistributionFunction("offer", null);//no need to listen to "offer" message anymore
+                    connection_manager.server_socket.send(">"+JSON.stringify({tag: "answer", answer: answer}));
+                });
+            });
+        });
+    }
+    //ice candidate
+    this.setDistributionFunction("candidate", function(message){//listen to "candidate" message
+        if(message.candidate){
+            connection_manager.peerConnection.addIceCandidate(message.candidate);
+        }
+        else{
+            connection_manager.setDistributionFunction("candidate", null);//stop listening
+        }
+    });
+    this.peerConnection.onicecandidate = function(e){
+        this.server_socket.send(">"+JSON.stringify({tag: "candidate", candidate: e.candidate}));//send candidate to the other one
+    }
+}
+connection_manager.closePeerConnection = function(){
+    this.dataChannel.close();
+    this.peerConnection.close();
+}
--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 //globle
 inputs = [[0,0,0,0],[0,0,0,0]];
 var base_objects = [];
-var server_socket = new WebSocket("wss://onescoop.info:443/Dodgem/play");
 var peerConnection;
 var dataChannel;
 var whohost = null;
+//
+connection_manager.socketServer = "onescoop.info:443/Dodgem/play";
+connection_manager.iceServers = [{urls: "stun:stun.xten.com"}, {urls: "stun:stun.sipgate.net:10000"},
+    {urls: "stun:stun.freeswitch.org"}, {urls: "turn:118.25.102.41:3478", username:"fourscoop", credential:"team2018"}];
+base_generator.scene_div = document.getElementById("scene_div");
+base_generator.render_scale = 16;
 //basic description of the word
 base_objects.push({x:0, y:0, width:20, height:20, angle:0, src:"point"}); //center point
 base_objects.push({x:0, y:200, width:40, height:80, angle:180, src:"", tag:"car"}); //p1body
@@ -16,9 +21,10 @@ base_objects.push({x:-300, y:0, width:10, height:600, angle:0, src:"", tag:"fixe
 base_objects.push({x:0, y:300, width:10, height:600, angle:90, src:"", tag:"fixed"}); //
 base_objects.push({x:0, y:-300, width:10, height:600, angle:90, src:"", tag:"fixed"}); //
 
-
-base_generator.scene_div = document.getElementById("scene_div");
-base_generator.render_scale = 16;
+connection_manager.startSocket();
+connection_manager.setDistributionFunction("pairing success", function(message){
+    prepareGame(message);
+})
 
 var timetick = function(deltaTime){
     if(whohost=="youhost") base_generator.fixedUpdate(deltaTime);
@@ -39,35 +45,9 @@ function render(){
 }
 requestAnimationFrame(render);
 
-server_socket.onmessage = function(e){
-    //console.log("receive a message from server:"+e.data);
-    var message = e.data;
-    if(e.data.charAt(0)!='{'){//this is an quick message
-        if(e.data.charAt(0)=='>'){
-            message = message.substring(1, message.length);
-        }
-        else if(e.data.charAt(0)==':'){
-            codec.decodeMotion(base_objects, e.data);
-            return;
-        }
-        else if(e.data.charAt(0)==';'){
-            codec.decodeInput(inputs[1], e.data);
-            return;
-        }
-    }
-    message = JSON.parse(message);
-    if(message.tag == "pairing success"){
-        prepareGame(message); return;
-    }
-    else if(message.tag == "offer"){
-        offerFunction(message); return;
-    }
-    else if(message.tag == "candidate"){
-        candidateFunction(message); return;
-    }
-}
 var offerFunction;
 var candidateFunction;
+server_socket = connection_manager.server_socket;
 server_socket.onopen = function(){
     console.log("socket open");
 }
@@ -80,97 +60,44 @@ server_socket.onerror = function(){
 function prepareGame(message){
     //hoster
     whohost = message.whohost;
-    //rtc
-    var ot = {iceServers: [{urls: "stun:stun.xten.com"}, {urls: "stun:stun.sipgate.net:10000"}
-    , {urls: "stun:stun.freeswitch.org"}]};
-    peerConnection = new RTCPeerConnection(ot);
-    peerConnection.onsignalingstatechange = function(e){
-        console.log("onsignalingstatechange:"+peerConnection.signalingState);
+    var starter = false;
+    if(whohost=="youhost")starter = true;
+    connection_manager.startPeerConnection(starter);
+    connection_manager.dataChannel.onerror = function(e){
+        console.log("channel error");
+        console.log(JSON.stringify(e));
     }
-    peerConnection.onconnectionstatechange = function(e){
-        console.log("onconnectionstatechange:"+peerConnection.connectionState);
-    }
-    peerConnection.oniceconnectionstatechange = function(e){
-        console.log("oniceconnectionstatechange:"+peerConnection.iceConnectionState);
-    }
-    peerConnection.onicegatheringstatechange = function(e){
-        console.log("onicegatheringstatechange:"+peerConnection.iceGatheringState)
-    }
-    peerConnection.onicecandidate = function(e){
-        //if(!e.candidate)e.candidate = {candidate: ""} ;
-        server_socket.send(">"+JSON.stringify({tag: "candidate", candidate: e.candidate}));//send candidate to the other one
-    }
-    candidateFunction = function(message){
-        //console.log("got ice candidate: "+JSON.stringify(message.candidate));
-        if(message.candidate==null)return ;
-        peerConnection.addIceCandidate(message.candidate).catch(function(reason){
-            console.log("addIceCandidate failed. reason: "+JSON.stringify(reason));
-        });
+    connection_manager.dataChannel.onclose = function(){
+        console.log("channel closed");
+        peerConnectionSendFunc = function(){};
     }
     if(whohost == "youhost"){
-        var dataoption = {ordered: false, maxRetransmitTime: 1};//, protocol:"DCT_RTP", maxRetransmits: 0
-        dataChannel = peerConnection.createDataChannel("gm", dataoption);
-        peerConnection.createOffer({offerToReceiveAudio: false, offerToReceiveVideo: false, voiceActivityDetection: false})//pc1 create offer
-        .then(function(offer){
-            peerConnection.setLocalDescription(offer);
-            server_socket.send(">"+JSON.stringify({tag: "offer", offer: offer}));//send offer to pc2
-        });
-        //offer response
-        offerFunction = function(message){
-            peerConnection.setRemoteDescription(message.offer);
-        }
         //channel
-        dataChannel.onopen = function(){
+        connection_manager.dataChannel.onopen = function(){
             console.log("channel open");
             peerConnectionSendFunc = function(){
-                dataChannel.send(codec.encodeMotion(base_objects));
+                connection_manager.dataChannel.send(codec.encodeMotion(base_objects));
             }
-            dataChannel.onerror = function(e){
-                console.log("channel error");
-                console.log(JSON.stringify(e));
-            }
-            dataChannel.onmessage = function(e){
-                //console.log("receive from p2p"+e.data);
-                var t = e.data.split(":");
-                console.log("receive pack: "+t[t.length-1]);
-                codec.decodeInput(inputs[1], e.data);
-            }
-            dataChannel.onclose = function(){
-                peerConnectionSendFunc = function(){};
-            }
+        }
+        connection_manager.dataChannel.onmessage = function(e){
+            //console.log("receive from p2p"+e.data);
+            var t = e.data.split(":");
+            console.log("receive pack: "+t[t.length-1]);
+            codec.decodeInput(inputs[1], e.data);
         }
     }
     else{
-        //offer response
-        offerFunction = function(message){
-            peerConnection.setRemoteDescription(message.offer);
-            peerConnection.createAnswer({offerToReceiveAudio: false, offerToReceiveVideo: false, voiceActivityDetection: false})//pc2 create answer
-            .then(function(offer){
-                peerConnection.setLocalDescription(offer);
-                server_socket.send(">"+JSON.stringify({tag: "offer", offer: offer}));//send offer to pc1
-            });
-        }
         //channel
-        peerConnection.ondatachannel = function(e){
-            dataChannel = e.channel;
-            dataChannel.onopen = function(){
-                console.log("channel open");
-                peerConnectionSendFunc = function(){
-                    dataChannel.send(codec.encodeInput(inputs[0]));
-                }
+        connection_manager.dataChannel.onopen = function(){
+            console.log("channel open");
+            peerConnectionSendFunc = function(){
+                connection_manager.dataChannel.send(codec.encodeInput(inputs[0]));
             }
-            dataChannel.onmessage = function(e){
-                var ttt = e.data.split(":");
-                console.log("receive from p2p"+ttt[ttt.length-1]+" time="+ new Date().getTime());
-                codec.decodeMotion(base_objects, e.data);
-            }
-            dataChannel.onerror = function(e){
-                console.log("channel error");
-                console.log(JSON.stringify(e));
-            }
-            dataChannel.onclose = function(){
-                peerConnectionSendFunc = function(){};
-            }
+        }
+        connection_manager.dataChannel.onmessage = function(e){
+            var ttt = e.data.split(":");
+            console.log("receive from p2p"+ttt[ttt.length-1]+" time="+ new Date().getTime());
+            codec.decodeMotion(base_objects, e.data);
         }
     }
 
@@ -179,85 +106,8 @@ function prepareGame(message){
     if(message.whohost == "youhost"){
         base_generator.generatePhysics();
     }
-    else{
-    }
 }
-//----------------------------------------------------------------------
-/*
-var ot = {iceServers: [{url: "stun:stun.freeswitch.org"}]};//{iceServers: [{url: "stun:stun.freeswitch.org"}]};
-var pc = new RTCPeerConnection(ot);
-var pcc = pc.createDataChannel("pc1c");
-var pc2 = new RTCPeerConnection(ot);
-var pc2c;
 
-var promise = pc.createOffer({offerToReceiveAudio: false, offerToReceiveVideo: false, voiceActivityDetection: false});
-var offer1, offer2;
-promise.then(
-    function(offer) {
-        offer1 = offer;
-        //console.log("offer1 created");
-        promise = pc.setLocalDescription(offer);
-        promise.then(function(){
-            //console.log("pc1 local set");
-            promise = pc2.setRemoteDescription(offer1);
-            promise.then(function(){
-                //console.log("pc2 remote set");
-                promise = pc2.createAnswer({offerToReceiveAudio: false, offerToReceiveVideo: false, voiceActivityDetection: false});
-                promise.then(function(offer){
-                    offer2 = offer;
-                    promise = pc2.setLocalDescription(offer2);
-                    promise.then(function(){
-                        promise = pc.setRemoteDescription(offer2);
-                        promise.then(function(){
-                            //console.log("offer signal finished");
-                            signaldone();
-                        })
-                    })
-                })
-            });
-        })
-    }
-);
-pc.onicecandidate = function(e){
-    if(!e.candidate)return;
-    pc2.addIceCandidate(e.candidate).then(()=>console.log("candidate scc"));
-    //console.log(JSON.stringify(e.candidate));
-}
-pc2.onicecandidate = function(e){
-    if(!e.candidate)return;
-    pc.addIceCandidate(e.candidate).then(()=>console.log("candidate scc"));
-    //console.log(JSON.stringify(e.candidate));
-}
-pc2.ondatachannel = function(e){
-    pc2c = e.channel;
-    pc2c.onmessage = function(e){
-        console.log("receive"+e.data);
-    }
-    pc2c.onopen = function(e){
-        console.log("pc2c open!");
-        console.log(pc2c.readyState);
-    }
-}
-pcc.onopen = function(e){
-    console.log("pc1c open!");
-    console.log(pcc.readyState);
-    pcc.send("hello");
-}
-pcc.onmessage = function(e){
-    console.log("receive"+e.data);
-}
-pcc.onclose = function(e){
-    console.log("pc1c close");
-}
-pcc.onerror = function(e){
-    console.log("pc1c error");
-}
-pc.onconnectionstatechange = function(e){
-    console.log("sss");
-}
-function signaldone(){
-}*/
-//------------------------------------------------------------
 {
     document.addEventListener("keydown", function(e){
         switch(e.keyCode){
@@ -273,21 +123,7 @@ function signaldone(){
             case 40:
                 inputs[0][3]=1;
                 break;
-        }/*
-        switch(e.keyCode){
-            case 65:
-                inputs[1][0]=1;
-                break;
-            case 87:
-                inputs[1][1]=1;
-                break;
-            case 68:
-                inputs[1][2]=1;
-                break;
-            case 83:
-                inputs[1][3]=1;
-                break;
-        }*/
+        }
         if(e.keyCode==70){
         }
     })
@@ -305,20 +141,6 @@ function signaldone(){
             case 40:
                 inputs[0][3]=0;
                 break;
-        }/*
-        switch(e.keyCode){
-            case 65:
-                inputs[1][0]=0;
-                break;
-            case 87:
-                inputs[1][1]=0;
-                break;
-            case 68:
-                inputs[1][2]=0;
-                break;
-            case 83:
-                inputs[1][3]=0;
-                break;
-        }*/
+        }
     })
 }
